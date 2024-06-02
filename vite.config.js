8c80e96a0dccf63647f91e018a6bcd9a8e61822d
piyushpise0333 <piyushpise0333@gmail.com>
2024-06-02 23:57:17 +0530

    -

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/interactive-card-details-form-react-vite",
+  base: "/Interactive-card-details-form-react-vite",
   plugins: [react()],
 })
